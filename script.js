0eb36bb8004ceb3bdc330ad9c1d7178a9e8c9126
RezaShakeri19 <rslanicad@gmail.com>
2025-04-10 13:20:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function showText()
+{
+  document.getElementById("message1").style.display = "block";
+  document.getElementById("button1").style.display = "none";
+  document.getElementById("header-text").style.display = "block";
+}
