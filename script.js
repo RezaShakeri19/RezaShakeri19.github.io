8c60417d7d7b94e33d772441bf066aecc23b2a33
RezaShakeri19 <rslanicad@gmail.com>
2025-04-10 13:56:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function showText() {
     const headerText = document.getElementById('header-text');
     const message1 = document.getElementById('message1');
     const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
 
     if (headerText.style.display === 'none') {
         headerText.style.display = 'block';
         message1.style.display = 'block';
-        button1.style.display = 'none';  // Hide button1 when clicked
+        button1.style.display = 'none'; 
+        button2.style.display = 'block';
     } else {
         headerText.style.display = 'none';
         message1.style.display = 'none';
-        button1.style.display = 'block';  // Show button1 again
+        button1.style.display = 'block'; 
+        button2.style.display = 'none';
     }
 }
